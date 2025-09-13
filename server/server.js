43a729ad56479a7c25a456c4ddf1c6d1dd0bc716
Nick Eversgerd <nickeversgerd@Nicks-MacBook-Pro.local>
2025-09-12 20:01:47 -0400

    Added backend using express

--- server/server.js
+++ server/server.js
@@ -2,21 +2,50 @@ const express = require('express');
 const cors = require('cors');
 const { processDocument } = require('./textProcessing/index');
 const path = require('path');
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "documents/"),
+    filename: (req, file, cb) => cb(null, file.originalname)
+})
+const upload = multer({ storage });
 
 
 const app = express();
 app.use(cors());
 
+// post selected file to backend
+app.post('/documents', upload.single("file"), async (req, res) => {
+    try {
+        const filePath = path.join(__dirname, "documents", req.file.originalname);
+
+        console.log("uploaded file path:", filePath);
+
+        // call google vision OCR
+        const textAnnotation = await processDocument(filePath);
+
+        // send extracted text back
+        res.json({
+            filename: req.file.originalname,
+            text: textAnnotation ? textAnnotation : "",
+        });
+    } catch (err) {
+        console.error("Proccessing failed: ", err);
+        res.status(500).json({ error: err.message });
+    }
+})
+
+/*
 app.get('/process/:file', async (req, res) => {
     try {
-        const docuemntPath = path.join(__dirname, 'documents', req.params.file);
+        const documentPath = path.join(__dirname, '..', 'documents', req.params.file);
         const result = await processDocument(documentPath);
 
-        const text = result.join(' ');
-        res.send(text);
+        res.send(result);
     } catch (err) {
+        console.error("Processing failed: ", err);
         res.status(500).json({ error: err.message });
     }
 });
-
+*/
 app.listen(3001, () => console.log('Server is running on pport 3001'));
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,67 @@
 import Image from "next/image";
 import { useState } from 'react';
 export default function Home() {
-  const [data, setData] = useState('');
+const [text, setText] = useState('');
 
-  fetch('http://localhost:3001/process/td_statement.png')
-    .then(res => res.json)
-    .then(data => console.log(data))
-  return (
-    <h1>test</h1>
+
+    const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (!files || files.length === 0) return;
+    
+        const file = files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const res = await fetch('http://localhost:3001/documents', {
+                method: "POST",
+                body: formData,
+            });
+
+            const data = await res.json();
+            setText(data.text);
+        } catch (err) {
+            console.error('Upload fialed:', err);
+        }
+    };
+
+return (
+    <div>
+        <h1>{text}</h1>
+        <input 
+            type='file' 
+            style={{color: 'blue'}} 
+            onChange={handleChange}
+            multiple={false}>
+        </input>
+    </div>
   );
 }
+
+/*
+const files = e.target.files;
+        if (!files || files.length === 0) return;
+    
+        const file = files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const res = await fetch('http://localhost:3001/documents', {
+                method: "POST",
+                body: formData,
+            });
+
+            if (!res.ok) throw new Error('Upload failed');
+            
+            const encodedName = encodeURIComponent(file.name);
+            const processRes = await fetch(`http://localhost:3001/process/${encodedName}`);
+
+            const result = await processRes.text()
+            setData(result)
+
+        } catch (err) {
+            console.error('Upload failed: ', err)
+        }
+        setFile(file.name);   
+*/
